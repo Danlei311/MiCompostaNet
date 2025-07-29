6172406395e1a72486061fd43a808de2c6f5797d
IDGS-904-22001661 <armandodanielr29@outlook.com>
2025-07-29 01:41:11 -0600

    endpoints de Compras

---
+++ MiComposta/Dto/CompraMaterialDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MiComposta.Dto
+{
+    public class CompraMaterialDto
+    {
+        public int IdMaterial { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
---
+++ MiComposta/Dto/CompraRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MiComposta.Dto
+{
+    public class CompraRequestDto
+    {
+        public int IdProveedor { get; set; }
+        public List<CompraMaterialDto> Materiales { get; set; }
+    }
+}
