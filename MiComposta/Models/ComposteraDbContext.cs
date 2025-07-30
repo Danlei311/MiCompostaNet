a6fb2db99888d64a4e421283f2dcfaeb9d2f546e
IDGS-904-22001661 <armandodanielr29@outlook.com>
2025-07-29 22:18:42 -0600

    correcion bdContext

--- MiComposta/Models/ComposteraDbContext.cs
+++ MiComposta/Models/ComposteraDbContext.cs
@@ -39,9 +39,7 @@ public partial class ComposteraDbContext : DbContext
 
     public virtual DbSet<Ventum> Venta { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=ASUS_ARMANDO; Initial Catalog=ComposteraDB; user id=sa; password=admin;TrustServerCertificate=true");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
