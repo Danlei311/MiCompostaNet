eca184145c088bafc79195fce1c5e56b2fbb6dcc
IDGS-904-22001033 <alberto.rt@outlook.com>
2025-07-31 16:02:03 -0600

    Servicio de Perfil de Cliente terminado - 31/07/2025

---
+++ MiComposta/Dto/UpdatePasswordRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MiComposta.Dto
+{
+    public class UpdatePasswordRequestDto
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
---
+++ MiComposta/Dto/UsuarioPerfilDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MiComposta.Dto
+{
+    public class UsuarioPerfilDto
+    {
+        public int IdUsuario { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+    }
+}
